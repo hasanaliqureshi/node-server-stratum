097cc65451e46416a1d908f78250df3ae3cee027
hasanaliqureshi <hasanali.zeds@gmail.com>
2019-01-14 00:33:10 +0500

    host changed

--- stratum.js
+++ stratum.js
@@ -1,7 +1,7 @@
 const Proxy = require("coin-hive-stratum");
 const proxy = new Proxy({
   // host: "18.222.106.239",
-  host : "18.222.106.239",
+  host : "localhost",
   port: 8888
 });
 proxy.listen(8892);
